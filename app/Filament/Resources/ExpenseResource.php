46e2e8fc2fea4b02f601deb0cdf566688f727c5f
Orhan OSMANOĞLU <orhan.osmanoglu@erdemhospital.com>
2023-12-01 14:57:51 +0300

    cüzdan eklendi

---
+++ app/Filament/Resources/ExpenseResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Expense;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\TimePicker;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\ExpenseResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ExpenseResource\RelationManagers;
+
+class ExpenseResource extends Resource
+{
+    protected static ?string $model = Expense::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('description')->label('Açıklama')->required(),
+                TextInput::make('amount')->numeric()->prefix('₺')->label('Miktar')->required(),
+                DatePicker::make('date')->format('Y/m/d')->default(date('Y/m/d'))->label('Tarih')->required(),
+                Radio::make('type')->label('Tür')->inlineLabel(false)->required()
+                    ->options([
+                        'income' => 'Gelir',
+                        'income_next' => 'Gelecek Gelir',
+                        'expense' => 'Gider',
+                        'expense_next' => 'Gelecek Gider',
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('description')->label('Açıklama'),
+                TextColumn::make('amount')->label('Miktar'),
+                TextColumn::make('type')
+                    ->label('Tür')
+                    ->badge()
+                    ->color(fn (string $state): string => match ($state) {
+                        'income' => 'success',
+                        'expense' => 'danger',
+                        'income_next' => 'gray',
+                        'expense_next' => 'warning',
+                    }),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListExpenses::route('/'),
+            'create' => Pages\CreateExpense::route('/create'),
+            'edit' => Pages\EditExpense::route('/{record}/edit'),
+        ];
+    }
+}
